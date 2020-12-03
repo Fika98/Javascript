11a9ae30902811aafba52a4c54530b8fd3b4fbbf
Fika98 <Afzalov98@gmail.com>
2020-12-03 16:18:20 -0500

    done

---
+++ checkIfStringEmpty.js
@@ -0,0 +1,10 @@
+const checkIfStringIsEmpy = someString => {
+    if(someString == ""){
+        return true
+    }else{
+        return false
+    }
+}
+
+console.log(checkIfStringIsEmpy("hello"))
+console.log(checkIfStringIsEmpy(""))
